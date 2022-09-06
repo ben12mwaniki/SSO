d046d17dd7b4d58ba0842f563831796829f59807
Ben Mwaniki <61124184+ben12mwaniki@users.noreply.github.com>
2022-09-06 15:45:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("./routes/api");
 const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
+const routes = require("./routes/api");
+
 const app = express();
 
 const port = process.env.PORT;
